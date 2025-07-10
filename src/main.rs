a858da1b1f209fd800dd811b060ca585e7c569f5
rshke <pierre.ke.null@gmail.com>
2025-07-10 18:21:14 +0800

    add clippy run; should ci should failed on unused variable(x) in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
+    let x = 42;
     println!("Hello, world!");
 }
--- tests/ci.rs
+++ tests/ci.rs
@@ -1,4 +1,9 @@
+// #[test]
+// fn it_does_not_work() {
+//     assert_eq!(2 + 2, 3);
+// }
+
 #[test]
-fn it_does_not_work() {
-    assert_eq!(2 + 2, 3);
+fn it_does_work() {
+    assert_eq!(2 + 2, 4);
 }
