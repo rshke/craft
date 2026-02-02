f279e870fefbcc8253d8644c436a8fa3328fac6f
rshke <pierre.ke.null@gmail.com>
2026-02-02 22:08:42 +0800

    rerange project struct to reflect the rest api nature

--- src/routers/admin.rs
+++ src/routers/admin.rs
@@ -1,11 +1,24 @@
-pub mod dashboard;
-pub use dashboard::*;
+mod dashboard;
+use dashboard::*;
 
-pub mod password;
-pub use password::*;
+mod password;
+use password::*;
 
-pub mod logout;
-pub use logout::*;
+mod logout;
+use logout::*;
 
-pub mod newsletters;
-pub use newsletters::*;
+mod newsletters;
+use newsletters::*;
+
+use crate::app_state::AppState;
+use axum::routing::{get, post};
+use std::sync::Arc;
+
+pub fn router() -> axum::routing::Router<Arc<AppState>> {
+    axum::Router::new()
+        .route("/dashboard", get(admin_dashboard))
+        .route("/password", get(change_password_form))
+        .route("/password", post(change_password))
+        .route("/logout", post(logout))
+        .route("/newsletters", post(publish_newsletter))
+}
